ced3e375b9bb96537c6de984687788af14901e85
fikrireza <fikrirezaa@gmail.com>
2017-06-18 16:34:25 +0700

    bug fixed persetujuan akad

--- applications/app/Http/Controllers/AkadController.php
+++ applications/app/Http/Controllers/AkadController.php
@@ -9,6 +9,7 @@ use App\Models\Plafon;
 use App\Models\Anggota;
 use App\Models\Bmt;
 use App\Models\Pembayaran;
+use App\Models\Persetujuan;
 use App\Models\User;
 use App\Models\LogAkses;
 
@@ -118,7 +119,7 @@ class AkadController extends Controller
 
         $log = new LogAkses;
         $log->aksi = 'Membuat Akad baru '.$request->no_akad;
-        $log->aktor = 1;
+        $log->aktor = Auth::user()->id;
         $log->save();
 
 
@@ -134,9 +135,43 @@ class AkadController extends Controller
           abort(404);
         }
 
-        $getAkad->approved_by = Auth::user()->id;
-        $getAkad->approved_date = date('Y-m-d');
-        $getAkad->update();
+        DB::transaction(function () use($getAkad) {
+
+          $tahun = date('y');
+          $bulan = date('m');
+          $hari = date('d');
+          $rand = rand(1000,9999);
+
+          $no_persetujuan = Auth::user()->bmt->no_induk_bmt.'/APP/'.$tahun.'/'.$bulan.'/'.$hari.'/'.$rand;
+          $cek_no_persetujuan = Persetujuan::where('no_persetujuan', $no_persetujuan)->first();
+
+          if(!$cek_no_persetujuan){
+            $no_persetujuan;
+          }else{
+            $no_persetujuan = 'Kode Approve Akad BMT Habis - Contact Fikri Please';
+          }
+
+          $save = new Persetujuan;
+          $save->no_persetujuan = $no_persetujuan;
+          $save->tangal_persetujuan = date('Y-m-d');
+          $save->keterangan = 'Self Approved';
+          $save->id_akad = $getAkad->id;
+          $save->status_akad = 1;
+          $save->flag_status = 1;
+          $save->id_anggota = Auth::user()->id;
+          $save->id_aktor = Auth::user()->id;
+          $save->save();
+
+          $getAkad->approved_by = Auth::user()->id;
+          $getAkad->approved_date = date('Y-m-d');
+          $getAkad->update();
+
+          $log = new LogAkses;
+          $log->aksi = 'Menyetujui Akad '.$getAkad->kode_akad;
+          $log->aktor = Auth::user()->id;
+          $log->save();
+
+        });
 
         return redirect()->route('akad.index')->with('berhasil', 'Akad berhasil disetujui');
     }
